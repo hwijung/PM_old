87a0d96556bec6ec76da522ee27c558fdc6f6b93
hwijung <hwijung.ryu@gmail.com>
2015-06-27 08:52:10 +0900

    initial commit

--- PM/alarms/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.template import RequestContext
-from django.shortcuts import render, render_to_response, get_object_or_404
-
-# Create your views here.
-def main(request):
-	variables = RequestContext( request )
-	return render_to_response ( 'index.html', variables )
